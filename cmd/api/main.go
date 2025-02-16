adc37a3e893af09487bbb075ff2c23be6fc9cffa
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-02-16 13:47:45 +0000

    implement repository pattern for db queries

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,17 @@ import (
 	"log"
 
 	"example.com/social-app/internal/env"
+	"example.com/social-app/internal/store"
 )
 
 func main() {
 	cfg := config{addr: env.GetString("ADDR", ":8080")}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
