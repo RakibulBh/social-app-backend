9f0061a2f407e5f4b1fdd140f40fc11a7f962945
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-02-16 18:30:15 +0000

    handle user post creation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -40,6 +40,11 @@ func (app *application) mount() http.Handler {
 	// Route group
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+		})
+
 	})
 
 	// posts
